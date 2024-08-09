38621fa4dbd24fd468a9c32f313c1d00edbfaac0
Joe Walton <joseph.walton@amco-group.com>
2024-08-09 14:48:12 +0100

    basic authentication and testing
    Added a very basic way to authenticate the user and a user test to see if a piece of data in a test table can be found

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace DeveloperSkillsTracker
+﻿using System.Data;
+namespace DeveloperSkillsTracker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Developer Skills Tracker!\nPlease enter your username:");
+            SqlConnector dbConnection = new SqlConnector();
+            Console.Write("Welcome to the Developer Skills Tracker!\n");                       
+
+            while (true)
+            {
+                Console.Write("Username: ");
+                string username = Console.ReadLine();
+                Console.Write("Password: ");
+                string password = Console.ReadLine();
+                DataTable userTable = dbConnection.ExecuteQuery($"SELECT * FROM dbo.devUser WHERE userName = '{username}' AND userPass= '{password}'");
+
+                try 
+                {
+                    string dbUsername = (string)userTable.Rows[0]["userName"];
+                    string dbPassword = (string)userTable.Rows[0]["userPass"];
+                    break;
+                }
+                //userTable.Rows[0]["userName"] != "" && userTable.Rows[0]["userName"] != "")
+                catch
+                {
+                    Console.WriteLine("Invalid username or password. Please try again.");
+                }
+            }
+
+            //devUser user = new devUser(username);
         }
     }
 }
