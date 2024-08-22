827c161d39464b49f8648b444e5ac38c2821520a
Joe Walton <joseph.walton@amco-group.com>
2024-08-22 16:09:17 +0100

    Added comments to Program.cs
    Added general comments to help others understand what I'm doing

--- Program.cs
+++ Program.cs
@@ -12,43 +12,49 @@ namespace DeveloperSkillsTracker
     {
         public static void Main(string[] args)
         {
+            //Create variables that will be used throughout the program
             var context = new MyDbContext();
             bool exitProgram = false;
             bool backPressed = false;            
-
             string currentUsername;
             int currentUserID;
+
+            //A large ascii art header
             AnsiConsole.Write(
                 new FigletText("Developer Skills Tracker").Centered().Color(Color.RosyBrown));            
 
+            //The primary loop stopping the program from ending
             while (!exitProgram)
             {
+                //The landing page that asks for login details until correct details are provided
                 DimUser userDetails = Menus.MainMenu(context);
 
+                //Creating the initial user profile with data from the database
                 currentUsername = userDetails.Username;
                 currentUserID = userDetails.User_ID;
-
                 List<DimSkill> skillsList = context.Skills.Where(s => s.User_ID == currentUserID).ToList();
                 List<DimExperience> experiencesList = context.Experiences.Where(e => e.User_ID == currentUserID).ToList();
                 List<DimCertification> certificationsList = context.Certifications.Where(c => c.User_ID == currentUserID).ToList();
-
                 UserProfile profile = new UserProfile(userDetails.Username, userDetails.User_ID, skillsList, experiencesList, certificationsList);
 
                 Console.Clear();
 
                 bool inTableViewer = true;
 
+                //Loop allowing the user to go back to viewing all of the tables when going back from the data viewer menu
                 while (inTableViewer)
                 {
-                    //Refresh Lists
+                    //Refresh user profile with data from the database in case of changes
                     skillsList = context.Skills.Where(s => s.User_ID == currentUserID).ToList();
                     experiencesList = context.Experiences.Where(e => e.User_ID == currentUserID).ToList();
                     certificationsList = context.Certifications.Where(c => c.User_ID == currentUserID).ToList();
                     profile.Skills = skillsList;
                     profile.Experiences = experiencesList;
                     profile.Certifications = certificationsList;
+
                     string attributeChoice = Menus.TableViewer(context, profile);
 
+                    //Ends the main loop when the user chooses to exit, going back to the landing page
                     if (attributeChoice == "Exit")
                     {                      
                         exitProgram = true;
@@ -58,9 +64,10 @@ namespace DeveloperSkillsTracker
 
                     bool inDataViewer = true;
 
+                    //Loop allowing the user to go back to viewing specific attribute data when going back from altering the table
                     while (inDataViewer)
                     {
-                        ////Refresh Lists
+                        ////Refresh Lists in case of changes
                         skillsList = context.Skills.Where(s => s.User_ID == currentUserID).ToList();
                         experiencesList = context.Experiences.Where(e => e.User_ID == currentUserID).ToList();
                         certificationsList = context.Certifications.Where(c => c.User_ID == currentUserID).ToList();
@@ -74,6 +81,7 @@ namespace DeveloperSkillsTracker
 
                         Console.Clear();
 
+                        //Checks users latest choice and performs the appropriate action before going back to the start of the loop
                         if (changeChoice == "Add")
                         {
                             Menus.AddMenu(context, profile, attributeChoice, changeChoice);
